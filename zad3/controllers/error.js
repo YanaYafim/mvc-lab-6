3b98a445395b6ba51ef734d20f763e8466ffcd4e
YanaYafim <158202884+YanaYafim@users.noreply.github.com>
2024-04-23 11:07:33 +0200

    Delete zad3 directory

--- zad3/controllers/error.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    getNotFoundPage: (req, res) => {
-        res.status(404).render('not-found', { title: 'Page Not Found' });
-    }
-};
--- zad3/routes/error.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const errorController = require('../controllers/error');
-
-router.use(function(req, res, next) {
-    res.status(404).render('not-found', { title: 'Page Not Found' });
-});
-
-module.exports = router;
